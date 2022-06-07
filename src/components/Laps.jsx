3cfb610049942350df74fe46bf8e103d5df9a686
hyebinjo <hyebinjo91@gmail.com>
2022-06-07 23:17:17 +0900

    feat: 최단, 최장 기록 강조 효과

--- src/components/Laps.jsx
+++ src/components/Laps.jsx
@@ -1,4 +1,8 @@
 const Laps = ({ laps }) => {
+    const lapTimes = laps.reduce((prev, curr) => [...prev, curr[1]], []);
+    const minIdx = lapTimes.lastIndexOf(Math.min(...lapTimes));
+    const maxIdx = lapTimes.lastIndexOf(Math.max(...lapTimes));
+
     const formatTimeNum = (num) => {
         return num.toString().length === 1 ? `0${num}` : num;
     };
@@ -15,11 +19,26 @@ const Laps = ({ laps }) => {
             formatTimeNum(centisecond)
         );
     };
+
+    const setMinMax = (idx) => {
+        if (laps.length === 1) {
+            return;
+        }
+        if (idx === minIdx) return 'text-green-600';
+        if (idx === maxIdx) return 'text-red-600';
+        return;
+    };
+
     return (
         <article className="text-gray-600 h-64 overflow-auto border-t-2">
             <ul>
-                {laps.map((lap) => (
-                    <li className="flex justify-between py-2 px-3 border-b-2">
+                {laps.map((lap, idx) => (
+                    <li
+                        key={idx}
+                        className={`flex justify-between py-2 px-3 border-b-2 ${setMinMax(
+                            idx
+                        )}`}
+                    >
                         <span>랩 {lap[0]}</span>
                         <span>{formatTime(lap[1])}</span>
                     </li>
